56d51057ac13cc4ed230e5b63f011c255bc6415c
Souvik <souvikmandal210@gmail.com>
2023-07-22 21:23:38 +0530

    2nd commit

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.write("Hello World!");
+    res.write("1 ");
+    res.end('Response ends here!');
 })
 
 app.listen(port, () => {
