0c95fa8c02dc2422113be1b38eed56cf1fa8c616
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2023-07-28 21:29:02 +0530

    Updated so that existing image will be removed first

--- docs/jenkins-build.sh
+++ docs/jenkins-build.sh
@@ -1,7 +1,8 @@
-git pull origin main                                    # pull src code from github
-sudo docker build -t demo-server-dockerized .           # build docker image from that src code
 sudo docker ps -a | grep "nodejs-image-demo" &&         # if docker container named "nodejs-image-demo" exists go to next
 sudo docker stop nodejs-image-demo &&                   # stop the container named "nodejs-image-demo", if success got to next
-sudo docker rm nodejs-image-demo                        # remove the container named "nodejs-image-demo"
-sudo docker run --name nodejs-image-demo -p 3001:3000 -d demo-server-dockerized
+sudo docker rm nodejs-image-demo  &&                    # remove the container named "nodejs-image-demo"
+sudo docker images | grep "demo-server-dockerized" &&   # if image named "demo-server-dockerized" exists go to next
+sudo docker rmi demo-server-dockerized                  # remove the image named "demo-server-dockerized"
+sudo docker build -t demo-server-dockerized .           # build docker image from that src code
+sudo docker run --name nodejs-image-demo -p 3001:5500 -d demo-server-dockerized
     # run a container from the image which has the tag: "demo-server-dockerized", name it "nodejs-image-demo", and bind port of 3001:external with 3000:internal
