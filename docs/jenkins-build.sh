49cd1ac4e4c8fbfc4cdad32acf264d8ab7ab90d4
Souvik <souvikmandal210@gmail.com>
2023-07-30 01:09:25 +0530

    kubernetes changes

--- docs/jenkins-build.sh
+++ docs/jenkins-build.sh
@@ -1,9 +1,9 @@
-git pull origin main                                    # pull src code from github
-sudo docker ps -a | grep "nodejs-image-demo" &&         # if docker container named "nodejs-image-demo" exists go to next
-sudo docker stop nodejs-image-demo &&                   # stop the container named "nodejs-image-demo", if success got to next
-sudo docker rm nodejs-image-demo		                # remove the container named "nodejs-image-demo"
-sudo docker images | grep "demo-server-dockerized" &&   # if image named "demo-server-dockerized" exists go to next
-sudo docker rmi demo-server-dockerized                  # remove the image named "demo-server-dockerized"
-sudo docker build -t demo-server-dockerized .           # build docker image from that src code
-sudo docker run --name nodejs-image-demo -p 3001:3000 -d demo-server-dockerized
-    # run a container from the image which has the tag: "demo-server-dockerized", name it "nodejs-image-demo", and bind port of 3001:external with 3000:internal
+# git pull origin main                                    # pull src code from github
+sudo docker ps -a | grep "nodejs-demo-container" &&         # if docker container named "nodejs-demo-container" exists go to next
+sudo docker stop nodejs-demo-container &&                   # stop the container named "nodejs-demo-container", if success got to next
+sudo docker rm nodejs-demo-container		                # remove the container named "nodejs-demo-container"
+sudo docker images | grep "nodejs-demo-image" &&            # if image named "nodejs-demo-image" exists go to next
+sudo docker rmi nodejs-demo-image                           # remove the image named "nodejs-demo-image"
+sudo docker build -t nodejs-demo-image .                    # build docker image from that src code
+sudo docker run --name nodejs-demo-container -p 3001:3000 -d nodejs-demo-image
+    # run a container from the image which has the tag: "nodejs-demo-image", name it "nodejs-demo-container", and bind port of 3001:external with 3000:internal
